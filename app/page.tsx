ad284af6e7ea827557c40cfdc21bfb9733bf6a9f
OmarDugha <116832203+OmarDugha03@users.noreply.github.com>
2024-03-28 06:50:18 +0300

    .

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 import Hero, { products } from '@/components/Hero'
 import CallTOAction from '@/components/callToAction'
 import Card from '@/components/ui/card'
+import { LampContainer, LampDemo } from '@/components/ui/lamp'
 import { TracingBeam } from '@/components/ui/tracing-beam'
 export default function Home() {
   return (
@@ -9,8 +10,10 @@ export default function Home() {
       <div className='pointer-events-none absolute  inset-0 flex items-center justify-center  bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]'></div>
       <TracingBeam className=' lg:mt-20'>
         <Hero />
-        <p className='ms-4 text-3xl'>My Projects : </p>
-        <section className='grid w-[100%] grid-cols-1 gap-5 md:grid-cols-2   '>
+        <div className=' w-full'>
+          <LampDemo text1={'My Projects'} />
+        </div>
+        <section className='mt-0 grid w-[100%] grid-cols-1 gap-5 md:grid-cols-2   '>
           {products.map(item => (
             <Card
               title={'this is the title'}
---
+++ components/ui/lamp.tsx
@@ -0,0 +1,94 @@
+'use client'
+import React from 'react'
+import { motion } from 'framer-motion'
+import { cn } from '@/lib/utils'
+
+export function LampDemo({ text1, text2 }: { text1: string; text2?: string }) {
+  return (
+    <LampContainer className='bg-transparent'>
+      <motion.h1
+        initial={{ opacity: 0.5, y: 100 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: 'easeInOut'
+        }}
+        className=' bg-gradient-to-br from-slate-300 to-slate-500 bg-clip-text py-4 text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl'
+      >
+        <span className=''>{text1}</span>
+        <br /> {text2}
+      </motion.h1>
+    </LampContainer>
+  )
+}
+
+export const LampContainer = ({
+  children,
+  className
+}: {
+  children: React.ReactNode
+  className?: string
+}) => {
+  return (
+    <div
+      className={cn(
+        'relative z-0 flex min-h-[60vh] w-full flex-col items-center justify-center overflow-hidden rounded-md ',
+        className
+      )}
+    >
+      <div className='relative isolate z-0 flex w-full flex-1 scale-y-125 items-center justify-center '>
+        <motion.div
+          initial={{ opacity: 0.5, width: '15rem' }}
+          whileInView={{ opacity: 1, width: '30rem' }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: 'easeInOut'
+          }}
+          style={{
+            backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`
+          }}
+          className='bg-gradient-conic absolute inset-auto right-1/2 h-56 w-[30rem] overflow-visible from-cyan-500 via-transparent to-transparent text-white [--conic-position:from_70deg_at_center_top]'
+        ></motion.div>
+        <motion.div
+          initial={{ opacity: 0.5, width: '15rem' }}
+          whileInView={{ opacity: 1, width: '30rem' }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: 'easeInOut'
+          }}
+          style={{
+            backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`
+          }}
+          className='bg-gradient-conic absolute inset-auto left-1/2 h-56 w-[30rem] from-transparent via-transparent to-cyan-500 text-white [--conic-position:from_290deg_at_center_top]'
+        ></motion.div>
+        <motion.div
+          initial={{ width: '8rem' }}
+          whileInView={{ width: '16rem' }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: 'easeInOut'
+          }}
+          className='absolute inset-auto z-30 h-44 w-64 translate-y-[3rem] rounded-full blur-2xl'
+        ></motion.div>
+        <motion.div
+          initial={{ width: '15rem' }}
+          whileInView={{ width: '30rem' }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: 'easeInOut'
+          }}
+          className='absolute inset-auto z-50 h-0.5 w-[30rem] -translate-y-[7rem] bg-cyan-400 '
+        ></motion.div>
+      </div>
+
+      <div className='relative z-50 flex -translate-y-80 flex-col items-center px-5'>
+        {children}
+      </div>
+    </div>
+  )
+}
